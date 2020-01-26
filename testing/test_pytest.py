29b2f189082f964f20cf6236131cda0603abd7c4
vignesh <you@example.com>
2020-01-26 21:37:43 +0530

    commiting testig source code

---
+++ testing/test_pytest.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+#driver.implicitly_wait(10)   
+
+def test_login():
+    driver=webdriver.Chrome("C:/Users/VigneshSmile/capstone-easyclaim-frontend/testing/chromedriver.exe")
+    driver.implicitly_wait(10)
+    driver.get('http://35.223.71.21/')
+    driver.maximize_window()
+    driver.find_element_by_id("username").send_keys('vishnu')
+    driver.find_element_by_id('pwd').send_keys('vishnu@123')
+    driver.find_element_by_xpath('/html/body/app-root/app-login/div/div/form/button').click()
+    driver.close()
+    
