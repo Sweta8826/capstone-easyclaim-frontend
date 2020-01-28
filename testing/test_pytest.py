1700ab88fa0d91db0bbdd037b9de042eb5bbbdfe
root <root@ip-172-31-27-32.ap-south-1.compute.internal>
2020-01-28 17:41:00 +0000

    Fixed the testing browser issue

--- testing/test_pytest.py
+++ testing/test_pytest.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
-#driver.implicitly_wait(10)   
+#driver.implicitly_wait(10)
+from selenium.webdriver.chrome.options import Options
+
+option=Options()
+option.add_argument("--headless")   
+option.add_argument('--no-sandbox')
+option.add_argument('start-maximized')
+option.add_argument('disable-infobars')
+option.add_argument('--disable-extensions')
+option.add_argument('--disable-dev-shm-usage')
 
 def test_login():
     global driver
-    driver=webdriver.Chrome("testing/chromedriver.exe")
+    driver=webdriver.Chrome("./testing/chromedriver", options=option)
     driver.implicitly_wait(10)
     driver.get('http://35.223.71.21/')
     driver.maximize_window()
